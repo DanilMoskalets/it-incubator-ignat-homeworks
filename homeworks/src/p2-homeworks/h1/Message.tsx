18161e58adfb190297edeb75b8947095c204c044
Данил <dan.moskalets@gmail.com>
2022-01-10 00:53:40 +0200

    Commit to main

--- homeworks/src/p2-homeworks/h1/Message.tsx
+++ homeworks/src/p2-homeworks/h1/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from './Message.module.css'
 
 type typeMessageProp = {
     avatar: string
@@ -9,8 +10,14 @@ type typeMessageProp = {
 
 export default function Message(props: typeMessageProp) {
     return (
-        <div>
-        <h1> sjfiojsf </h1>
+
+        <div className={s.message}>
+            <img src={props.avatar} className={s.avatar}/>
+            <div className={s.content}>
+                <h6 className={s.name} > {props.name} </h6>
+                <p> {props.message}</p>
+                <p>{props.time}</p>
+            </div>
         </div>
     )
 }
