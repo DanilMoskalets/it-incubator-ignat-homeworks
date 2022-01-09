1dec56860c30892a9d7b792cdbbf078b5faefb06
Данил <dan.moskalets@gmail.com>
2022-01-09 15:45:08 +0200

    Moved the project

---
+++ homeworks/src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type typeMessageProp = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+export default function Message(props: typeMessageProp) {
+    return (
+        <div>
+        <h1> sjfiojsf </h1>
+        </div>
+    )
+}
+
+
