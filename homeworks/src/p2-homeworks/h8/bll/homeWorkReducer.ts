0db9814f2fc7f63bf98c426bfff965c1b597814e
Данил <dan.moskalets@gmail.com>
2022-06-25 22:34:27 +0300

    feat: add Reducer

--- homeworks/src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ homeworks/src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,35 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export type sortTypeAT = {
+    type: 'SORT'
+    payload: string
+}
+
+
+export type sortAge = {
+    type: 'CHECK-AGE'
+    payload: number
+}
+
+type defaultAction = sortTypeAT |  sortAge
+
+export const homeWorkReducer = (state: UserType[], action: defaultAction): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': {
-            // need to fix
+        case 'SORT':
+            const copyState = [...state]
+            copyState.sort((a, b) => {
+                if (a.name > b.name) {
+                    return 1
+                } else if (a.name < b.name) {
+                    return -1
+                } else {
+                    return 0
+                }
+            })
+            return action.payload === 'up' ?  copyState : copyState.reverse()
+        case "CHECK-AGE":
+            return [...state].filter( s => s.age >= 18)
+        default:
             return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
     }
 }
